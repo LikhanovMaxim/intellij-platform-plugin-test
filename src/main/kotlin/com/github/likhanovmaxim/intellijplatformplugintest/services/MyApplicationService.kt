20cab93ed446fe9c005fb1f70402f425021e96c3
GitHub Action <action@github.com>
2021-09-20 12:33:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/likhanovmaxim/intellijplatformplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.likhanovmaxim.intellijplatformplugintest.services
+
+import com.github.likhanovmaxim.intellijplatformplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/likhanovmaxim/intellijplatformplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/likhanovmaxim/intellijplatformplugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.likhanovmaxim.intellijplatformplugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.likhanovmaxim.intellijplatformplugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
